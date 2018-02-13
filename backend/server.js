20b730aad4a96a8388e10e43bcbfab17eaa33c20
Masih Tabrizi <masih_tabrizi88@yahoo.com>
2018-02-14 01:18:31 +0330

    adding compile version to request

--- backend/server.js
+++ backend/server.js
@@ -12,19 +12,25 @@ app.use(bodyParser.text());
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Compile-Version");
     next();
 });
 
 app.post("/swiftrunner/playground", function(req, res) {
 
 	const source_code = req.body;
+    // TODO: pass this to compile with designated version
+    const compile_version = req.headers['compile-version']
     const rand = rn.generator({min:  1, max:  1000, integer: true})()
 
     // FIXME: check if the number already exist
     var source_file = config.storage_directory + "/" + rand + ".swift"
 
     fs.writeFile(source_file, source_code, function(error) {
-        if (error) { return console.log(error); }
+        if (error) {
+            res.sendStatus(500);
+            return console.log(error);
+        }
 
         var compile_script = exec('swift ' + source_file, function(error, stdout, stderr) {
             if (error !== null) {
